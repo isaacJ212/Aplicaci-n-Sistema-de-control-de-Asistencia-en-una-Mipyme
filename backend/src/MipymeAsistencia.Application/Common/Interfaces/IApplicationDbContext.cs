88fe43f340af5bd72722216756ca007faf5604c5
isaacJ212 <isaacejimenez2006@gmail.com>
2026-08-01 12:21:27 -0600

    feat(backend/autenticacion): se crewa lo que es IaplicationDbconetext

---
+++ backend/src/MipymeAsistencia.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using MipymeAsistencia.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MipymeAsistencia.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<Usuario> Usuarios { get; }
+    DbSet<Rol> Roles { get; }
+    DbSet<Empleado> Empleados { get; }
+    DbSet<HistorialAsistencia> HistorialAsistencias { get; }
+    DbSet<HistorialPermisoVacacion> HistorialPermisosVacaciones { get; }
+    DbSet<HistorialPlanilla> HistorialPlanillas { get; }
+    DbSet<HoraExtra> HorasExtras { get; }
+    DbSet<EvaluacionDesempeno> EvaluacionesDesempeno { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
