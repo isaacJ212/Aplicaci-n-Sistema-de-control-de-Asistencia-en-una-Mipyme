6cb4984dc9214c960bed1b32a628fba65733f647
isaacJ212 <isaacejimenez2006@gmail.com>
2026-08-01 17:25:16 -0600

    feat(backend/autenticacion): se agrega DbSet<ConfiguracionSede> a la interfaz IApplicationDbContext para que los handlers de Application puedan acceder a la tabla

--- backend/src/MipymeAsistencia.Application/Common/Interfaces/IApplicationDbContext.cs
+++ backend/src/MipymeAsistencia.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace MipymeAsistencia.Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
+    DbSet<ConfiguracionSede> ConfiguracionesSede { get; }
     DbSet<Usuario> Usuarios { get; }
     DbSet<Rol> Roles { get; }
     DbSet<Empleado> Empleados { get; }
