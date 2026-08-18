b626085e00b3278965371fc977dc955777e24c04
isaacJ212 <isaacejimenez2006@gmail.com>
2026-08-18 16:07:25 -0600

    feat(backend/refactor): se añadieron nuevso campos requeridos a los Dtos

--- backend/src/MipymeAsistencia.Application/Common/DTOs/Empleado/CreateEmpleadoRequestDto.cs
+++ backend/src/MipymeAsistencia.Application/Common/DTOs/Empleado/CreateEmpleadoRequestDto.cs
@@ -4,6 +4,9 @@ public class CreateEmpleadoRequestDto
 {
     public int IdUsuario { get; set; }
     public string CedulaIdentificacion { get; set; } = string.Empty;
+    public string NumeroInss { get; set; } = string.Empty;
+    public string EstadoCivil { get; set; } = "Soltero";
+    public string EstadoEmpleado { get; set; } = "Activo";
     public string? FotoUrl { get; set; }
     public string Nombres { get; set; } = string.Empty;
     public string Apellidos { get; set; } = string.Empty;
